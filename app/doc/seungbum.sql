b94b6ad7163ccb7f45d5ac97c992f490928682e4
Seung-Bum <sb910126@gmail.com>
2022-03-14 22:44:27 +0900

    sql 데이터 입력

--- app/doc/seungbum.sql
+++ app/doc/seungbum.sql
@@ -1,1 +1,111 @@
-.
+/* 문의 */
+insert into inquiry(
+mno,
+type, 
+title, 
+contents, 
+attach_file, 
+answer
+) values(
+1,
+1,
+'문의글 올립니다.',
+'궁금합니다.', 
+'askheufhiasehihaskuehfkuashefukshkushefk12412', 
+'대답입니다.'
+);
+
+insert into inquiry(
+mno,
+type, 
+title, 
+contents, 
+attach_file, 
+answer
+) values(
+1,
+1,
+'문의글을 다시 올립니다.',
+'궁금합니다!!!.', 
+'askheufhiasehihasasefasef11kuehfkuashefukshkushefk12412', 
+'대답입니다. 대답입니다.'
+);
+
+insert into inquiry(
+mno,
+type, 
+title, 
+contents, 
+attach_file, 
+answer
+) values(
+1,
+2,
+'문의글 문의글 문의글',
+'궁금합니다!!!????????', 
+'askheufhiasehihasasefasef11kuehfkuashefukshkushefk12412', 
+'대답입니다. 대답입니다. 대답입니다.'
+);
+
+insert into inquiry(
+mno,
+type, 
+title, 
+contents, 
+attach_file, 
+answer
+) values(
+1,
+2,
+'문의글 문의글 문의글',
+'궁금합니다!!!????????', 
+'askheufhiasehihasasefasef11kuehfkuashefukshkushefk12412', 
+'대답입니다. 대답입니다. 대답입니다.'
+);
+
+insert into inquiry(
+mno,
+type, 
+title, 
+contents, 
+attach_file, 
+answer
+) values(
+1,
+2,
+'문의글 문의글 문의글',
+'궁금합니다!!!????????', 
+'askheufhiasehihasasefasef11kuehfkuashefukshkushefk12412', 
+'대답입니다. 대답입니다. 대답입니다.'
+);
+
+insert into inquiry(
+mno,
+type, 
+title, 
+contents, 
+attach_file, 
+answer
+) values(
+1,
+2,
+'이용방법 설명 부탁드립니다.',
+'이용방법이 궁금해요 알려주세요.', 
+'askheufhiasehihasasefaseserfseff11kuehfkuashefukshkushefk12412', 
+'이용방법이 어렵지 않습니다. 한번 이용해보세요'
+);
+
+insert into inquiry(
+mno,
+type, 
+title, 
+contents, 
+attach_file
+) values(
+1,
+2,
+'문의글 문의글 문의글 문의글',
+'궁금합니다!!!????????@@@@', 
+'askheufhiasehihasasefasef11kuehfkuashefukshkushefk12412'
+);
+
