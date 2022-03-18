9fd7fed6057844b2c78b96e34ab4b10318ad8cc6
supercode333 <gacomonkey123@gmail.com>
2022-03-19 01:17:50 +0900

    음주관리 update

---
+++ app/src/main/java/com/bitproject/controller/AlcmgmtController.java
@@ -0,0 +1,43 @@
+package com.bitproject.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.bitproject.dao.AlcmgmtDao;
+import com.bitproject.domain.AlcoholManagement;
+
+@RestController
+public class AlcmgmtController {
+
+  @Autowired 
+  AlcmgmtDao alcmgmtDao;
+
+  @RequestMapping("/alcmgmt/list")
+  public Object list()  {
+    return alcmgmtDao.findAll(); 
+  }
+
+  @RequestMapping("/alcmgmt/add")
+  public Object add(AlcoholManagement alcmgmt)  {
+    return alcmgmtDao.insert(alcmgmt);
+  }
+
+
+  @RequestMapping("/alcmgmt/get")
+  public Object get(int no)  {
+    AlcoholManagement alcmgmt= alcmgmtDao.findByNo(no);
+    return alcmgmt != null ? alcmgmt : "";
+  }
+
+  @RequestMapping("/alcmgmt/update")
+  public Object update (AlcoholManagement alcmgmt)  {
+    return alcmgmtDao.update(alcmgmt);
+  }
+
+  @RequestMapping("/alcmgmt/delete")
+  public Object delete(int no)  {
+    return alcmgmtDao.delete(no);
+  }
+}
+
+
---
+++ app/src/main/java/com/bitproject/dao/AlcmgmtDao.java
@@ -0,0 +1,25 @@
+package com.bitproject.dao;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import com.bitproject.domain.AlcoholManagement;
+
+@Mapper
+public interface AlcmgmtDao {
+  int countAll();
+
+  List<AlcoholManagement> findAll();
+
+  int insert(AlcoholManagement alcmgmt);
+
+  AlcoholManagement findByNo(int no);
+
+  int update(AlcoholManagement alcmgmt);
+
+  int delete(int no);
+
+  // int increaseAmount(int no); 
+
+
+}
+
