812a50eecbaa9e85b7350911c45aa678acb00c9c
Seung-Bum <sb910126@gmail.com>
2022-03-22 15:56:41 +0900

    store 수정본 중간 push

--- app/src/main/java/com/bitproject/App.java
+++ app/src/main/java/com/bitproject/App.java
@@ -14,8 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class App {
 
   public static void main(String[] args) {
-    SpringApplication.run(App.class, args);   // springApplication 클래스의 run 메서드를 호출하는 것! App.class에서 .class는 확장자가 아니라 APP이라는 클래스의(자바에서 제공하는) 내장변수임! class 변수에는 해당 클래스의 정보가 들어가있다. //App이라는 클래스는 SpringApplication이라는 외부 라이브러리를 사용하는 것! ctrl 누른채로 SpringApplication 클릭하면 이 위치로 간다! 
-    // 스프링부트가 객체를 만든다! 따라서 App.java를 실행하면 ContactController.java에 있는 "ContactController() 호출됨!"이게 출력됨
+    SpringApplication.run(App.class, args);
   }
 
   //@Bean
--- app/src/main/resources/static/store/store.js
+++ app/src/main/resources/static/store/store.js
@@ -1,5 +1,51 @@
-const lightBtn = document.querySelectorAll('.store-category-sort');
-const btn = document.querySelector('.store-category-font:nth-child(3)');
+const lightBtn = document.querySelector('.store-category-sort');
+
+const btn1 = document.querySelector('.store-category-font:nth-child(1)');
+
+
+
+lightBtn.addEventListener("click",function(e){
+    console.log(e.target); == 전체
+});
+
+
+
+
+for(let i = 0; i < lightBtn.length; i++){
+    document.querySelector(`.store-category-font:nth-child(${i})`); 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+btnPush = [];
+btnNonPush = [];
+
+
+// btn1.onclick = function() { // 전체버튼
+//     console.log(btn1.getElementsByTagName("button"))
+// };
+
+
+
+
+// 함수에 보관
+// btn1.addEventListener("click" , (e) => {
+//     console.log(e.target)
+// })
+
+
+// btn1.addEventListener("click" , (e) => {
+//     console.log(e.target)
+// })
 
 
 
@@ -9,12 +55,33 @@ lightBtn.forEach(btn => {
 });
 
 
-for (let i = 0; i < lightBtn.length; i++) {
-    document.querySelector(`.store-category-font:nth-child(${i})`);
-
-}
 
 
-btn.onclick = function() {
-    console.log("ggggg")
-};
+
+
+
+
+// while (true) {
+//     for (let i = 0; i < lightBtn.length; i++) {
+//         catBtn = document.querySelector(`.store-category-font:nth-child(${i})`);
+//         catBtn.onclick = function() {
+//             console.log("ggggg")
+//         }
+//     }
+// }
+
+
+function hide(e){
+    console.log(e.currentTarget);
+    // When this function is used as an event handler: this === e.currentTarget
+  }
+
+// boxElem.addEventListener("click", logEvent);
+// for (let i = 0; i < lightBtn.length; i++) {
+//     document.querySelector(`.store-category-font:nth-child(${i})`);
+// }
+
+
+// btn.onclick = function() {
+//     console.log("ggggg")
+// };
