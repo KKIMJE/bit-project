dd4e8617bc20e38d05ea0115e92719e5a37ff3d9
ture9200 <didtjddms09@naver.com>
2022-03-24 02:55:29 +0900

    2022-03-23 사장님-주점관리

---
+++ app/src/main/resources/static/mypage(Member)/personalInfo.js
@@ -0,0 +1,18 @@
+function counter() {
+var content = document.getElementById('x-self-intro').value;
+if(content.length >300) {
+  alert("최대 300자까지 입력 가능합니다.");
+  content = content.substring(0,300);
+  document.getElementById('x-self-intro').value=content;
+} 
+//content += "<br>" + content.length + "/300자" ;
+// 요기서 textarea 안에 글자 count를 넣어준다. 그런데 textarea안에서 html tag가 동작 안하는 듯...
+document.getElementById('x-self-intro').innerHTML = content + "\n" + content.length+"/300자";
+ // document.getElementById('count').innerHTML = content.length +'/300자';
+ document.getElementById('x-self-intro').style.textAlign = "right";
+  
+}
+counter();
+function toggleImg() {
+    document.getElementById("ig").src = "../img/logo.png";
+  }
