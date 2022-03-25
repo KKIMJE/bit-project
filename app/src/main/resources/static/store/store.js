fee2997ab663af4695c944b1b9f8eb97af71fad6
Seung-Bum <sb910126@gmail.com>
2022-03-26 00:12:21 +0900

    store 카테고리 재수정, 전체버튼 수정 완료

--- app/src/main/resources/static/store/store.js
+++ app/src/main/resources/static/store/store.js
@@ -1,10 +1,11 @@
 const lightBtn = document.querySelector('.store-category-sort');
 
 lightBtn.addEventListener("click",function(e){
-    if (e.target == e.currentTarget) {
-      return;
-    } else {
-      e.currentTarget.querySelector('.act').classList.toggle('act');
-      e.target.classList.toggle('act');
-    }
-  });
+  if (e.target == e.currentTarget) {
+    return;
+  } else {
+    e.currentTarget.querySelector('.act').classList.toggle('act');
+    e.target.classList.toggle('act');
+  }
+});
+
