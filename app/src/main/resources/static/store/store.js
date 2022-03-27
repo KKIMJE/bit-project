5414f48b1862cb2f624aae73db8108f8299fc9f9
Seung-Bum <sb910126@gmail.com>
2022-03-27 19:20:35 +0900

    store next button modified

--- app/src/main/resources/static/store/store.js
+++ app/src/main/resources/static/store/store.js
@@ -1,5 +1,5 @@
+// category-tab
 const lightBtn = document.querySelector('.store-category-sort');
-
 lightBtn.addEventListener("click",function(e){
   if (e.target == e.currentTarget) {
     return;
@@ -9,3 +9,29 @@ lightBtn.addEventListener("click",function(e){
   }
 });
 
+// next, pre button
+const next = document.querySelector('.next-store');
+
+let storeAll = document.querySelectorAll('.store-contents-imgCard');
+let cursor = 1;
+let endPage = storeAll.length;
+
+for (let i=1; i < storeAll.length; i++) {
+  storeAll[i].style.display = "none";
+}
+
+next.addEventListener("click", () => {
+  if (storeAll[cursor].style.display == "none") {
+    storeAll[cursor].style.display = "flex"
+  }
+  for (let i=cursor+1; i < endPage; i++) {
+    storeAll[i].style.display = "none";
+  }
+  for (let i=cursor-1; i < cursor; i--) {
+    if (i == -1) {
+      break;
+    };
+    storeAll[i].style.display = "none";
+  }
+  cursor += 1;
+});
