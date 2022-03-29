ba5f845241147956e750e630dd548fb314876897
KJEzorro <gndlg91@gmail.com>
2022-03-29 21:45:52 +0900

    .

--- app/src/main/resources/static/alcohol/alcohollist.js
+++ app/src/main/resources/static/alcohol/alcohollist.js
@@ -2,57 +2,179 @@ const lightBtn = document.querySelector('.category-sort-div');
 var listDiv = document.querySelector(".alcohol-list-div")
 var itemDiv = document.querySelector(".alclist-item-div")
 
+function makeItemDiv() {
+  if (i % 5 == 0) {
+    var no = 1
+    var itemDiv = document.createElement("div")
+    itemDiv.classList.add(`"alclist-item-div${no}"`)
+    itemDiv.classList.add("d-flex")
+    itemDiv.classList.add("flex-row")
+    no++
+    listDiv.appendChild(itemDiv)
+  }
+}
+
+
+function allList() {
+  fetch("/alcohol/list")
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(alcohols) {
+      for (var i = 0; i < alcohols.length; i++) {
+        if (i % 5 == 0) {
+          var no = 1
+          var itemDiv = document.createElement("div")
+          itemDiv.classList.add(`"alclist-item-div${no}"`)
+          itemDiv.classList.add("d-flex")
+          itemDiv.classList.add("flex-row")
+          no++
+          listDiv.appendChild(itemDiv)
+        }
+
+        var div = document.createElement("div")
+        div.classList.add("card")
+        div.classList.add("border-white")
+        div.innerHTML = `
+          <a href="alcoholdetail.html?no=${alcohols[i].no}">
+            <img src="${alcohols[i].img}" class="card-img-top">
+            <div class="card-body">
+              <p class="card-text">
+              <ul>
+                <li>${alcohols[i].name}</li>
+                <li>${alcohols[i].degree}%</li>
+              </ul>
+              </p>
+            </div>
+          </a>
+        `
+        itemDiv.appendChild(div)
+      }
+    })
+}
+
+
+
+function targetList(targetNo) {
+  fetch("/alcohol/list")
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(alcohols) {
+      var count = 0
+      for (var i = 0; i < alcohols.length; i++) {
+        if (targetNo == alcohols[i].alcoholTypeNo) {
+
+
+          if (count % 5 == 0) {
+            var no = 1
+            var itemDiv = document.createElement("div")
+            itemDiv.classList.add(`"alclist-item-div${no}"`)
+            itemDiv.classList.add("d-flex")
+            itemDiv.classList.add("flex-row")
+            no++
+            listDiv.appendChild(itemDiv)
+          }
+
+
+
+          var div = document.createElement("div")
+          div.classList.add("card")
+          div.classList.add("border-white")
+          div.innerHTML = `
+        <a href="alcoholdetail.html?no=${alcohols[i].no}">
+        <img src="${alcohols[i].img}" class="card-img-top">
+        <div class="card-body">
+        <p class="card-text">
+        <ul>
+        <li>${alcohols[i].name}</li>
+        <li>${alcohols[i].degree}%</li>
+        </ul>
+        </p>
+        </div>
+        </a>
+        `
+          itemDiv.appendChild(div)
+          count++
+        }
+      }
+    })
+
+}
+
+
+
+
+
+
 lightBtn.addEventListener("click", function(e) {
+  $('.alcohol-list-div div').empty()
+
+
   if (e.target == e.currentTarget) {
     return;
   } else {
     e.currentTarget.querySelector('.act').classList.toggle('act');
     e.target.classList.toggle('act');
 
+    var targetNo = e.target.value
 
-    console.log(e.target.value);
-
-    fetch("/alcohol/list")
-      .then(function(response) {
-        return response.json()
-      })
-      .then(function(alcohols) {
-
-        for (var i = 0; i < alcohols.length; i++) {
-          if (e.target.value == alcohols[i].alcoholTypeNo) {
-            // if (i % 5 == 0) {
-            //   var no = 1
-            //   var itemDiv = document.createElement("div")
-            //   itemDiv.classList.add(`"alclist-item-div${no}"`)
-            //   itemDiv.classList.add("d-flex")
-            //   itemDiv.classList.add("flex-row")
-            //   no++
-            //   listDiv.appendChild(itemDiv)
-            // }
+    if (targetNo == 0) {
+      allList()
+    }
+    if (targetNo != 0) {
+      targetList(targetNo)
+    }
 
 
-            var div = document.createElement("div")
-            div.classList.add("card")
-            div.classList.add("border-white")
-            div.innerHTML = `
-            <a href="alcoholdetail.html?no=${alcohols[i].no}">
-              <img src="${alcohols[i].img}" class="card-img-top">
-              <div class="card-body">
-                <p class="card-text">
-                <ul>
-                  <li>${alcohols[i].name}</li>
-                  <li>${alcohols[i].degree}%</li>
-                </ul>
-                </p>
-              </div>
-            </a>
-          `
-            itemDiv.appendChild(div)
-          }
-        }
-      })
+
+    // $('.alcohol-list-div div').empty()
+
+
+    // console.log(e.target.value);
+
+    // fetch("/alcohol/list")
+    //   .then(function(response) {
+    //     return response.json()
+    //   })
+    //   .then(function(alcohols) {
+    //     $('.alcohol-list-div div').empty()
+    //     var count = 0;
+    //     for (var i = 0; i < alcohols.length; i++) {
+    //       var itemDiv = document.querySelector(".alclist-item-div")
+    //       if (e.target.value == alcohols[i].alcoholTypeNo) {
+    //         count++
+    //
+    //         if (count == 5) {
+    //           makeItemDiv();
+    //         }
+    //
+    //         var div = document.createElement("div")
+    //         div.classList.add("card")
+    //         div.classList.add("border-white")
+    //         div.innerHTML = `
+    //         <a href="alcoholdetail.html?no=${alcohols[i].no}">
+    //           <img src="${alcohols[i].img}" class="card-img-top">
+    //           <div class="card-body">
+    //             <p class="card-text">
+    //             <ul>
+    //               <li>${alcohols[i].name}</li>
+    //               <li>${alcohols[i].degree}%</li>
+    //             </ul>
+    //             </p>
+    //           </div>
+    //         </a>
+    //       `
+    //         itemDiv.appendChild(div)
+    //       }
+    //     }
+    //   })
   }
 });
+
+
+
+
 
 
 
