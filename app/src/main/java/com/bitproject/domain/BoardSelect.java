dfb723f5399c04bf949ff8fd80ab389ff5564a97
ture9200 <didtjddms09@naver.com>
2022-04-05 15:29:24 +0900

    storeChange 폰트크기 수정

--- app/src/main/java/com/bitproject/domain/BoardSelect.java
+++ app/src/main/java/com/bitproject/domain/BoardSelect.java
@@ -1,30 +1,15 @@
 package com.bitproject.domain;
 
+import lombok.Data;
+
+@Data
 public class BoardSelect{
 
   int mNo;
   int boardNo;
 
 
-  @Override
-  public String toString() {
-    return "BoardSelect [mNo=" + mNo + ", boardNo=" + boardNo + "]";
-  }
-
-
-  public int getmNo() {
-    return mNo;
-  }
-  public void setmNo(int mNo) {
-    this.mNo = mNo;
-  }
-  public int getBoardNo() {
-    return boardNo;
-  }
-  public void setBoardNo(int boardNo) {
-    this.boardNo = boardNo;
-  }
-
+ 
 
 
 }
