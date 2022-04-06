810ceee62a30f9be7a9689f12f73f87d6c9f74d8
Seung-Bum <sb910126@gmail.com>
2022-04-06 20:26:38 +0900

    Store SQL insert update

---
+++ app/doc/sampledata/store(22-04-06).sql
@@ -0,0 +1,99 @@
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(1, 1, 'dfgdsdddssas', '코다차야', '서울 강남구 강남대로 468', '02-260-0817', '11:11:11', '우아하고 섬세한 주점입니다.', 4, 1, 35, 38.0, 38.0, 'tissue1', 1, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(2, 2, 'asqweeeed', '온달집', '서울 강남구 강남대로152길 28', '031-260-0817', '22:59:11', NULL, 4, 1, 50, 37.0, 37.0, 'tissue2', 1, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(3, 3, 'qwerqwerqwer', '묵전', '서울 강남구 언주로168길 22', '02-261-0817', '22:59:11', '삐까뻔쩍 화려한 주점입니다.', 0, 1, 20, 36.0, 36.0, 'tissue3', 1, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(4, 4, 'mnmnmnnnnmnm', '마코토', '서울 강남구 압구정로80길 37', '02-260-0817', '22:59:11', '어둡고 위험한 주점입니다.', 2, 0, NULL, 35.0, 35.0, 'tissue4', 0, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(5, 5, 'ilillliililililili', '이치에', '서울 강남구 선릉로155길 23-3', '02-260-0817', '22:59:11', '호호하하 주점입니다.', 5, 1, 60, 33.0, 33.0, 'tissue5', 0, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(6, 123123, '123123123', '한잔의추억', '서울 강남구 논현로175길 68', '02-123-123', '13:12:12', 'gogo주점입니다.', 1, 1, 70, 34.0, 35.0, '123123', 0, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(7, 123123, '123123123', '이자카야나무', '서울 강남구 강남대로78길 8', '02-123-123', '16:12:11', 'gogo주점입니다.', 5, 1, 70, 34.0, 35.0, '123123', 1, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(8, 123123, '123123', '아트몬스터', '서울 강남구 테헤란로1길 28-3 1', '02-123-123', '15:12:12', 'gogo주점입니다.', 3, 1, 70, 34.0, 34.0, '123123', 1, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(9, 123123, '1231251sdiuhgusi', '뻐구기', '서울 강남구 도산대로17길 4', '02-260-0817', '13:12:12', 'yaya주점입니다.', 5, 0, 60, 34.0, 34.0, '123123', 0, 0);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(10, 123123, '456456', '수다', '서울 강남구 선릉로157길 26 1', '02-260-0817', '13:12:12', 'roro주점입니다.', 5, 1, 50, 34.0, 34.0, '123123', 1, 0);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(11, 456456, '456456', '아카라', '서울 강남구 봉은사로43길 14', '02-260-0817', '16:12:11', 'popo주점입니다.', 5, 0, 55, 34.0, 34.0, '123123', 1, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(15, 456456, '456456', '도연하다', '서울 강남구 논현로32길 5', '02-260-0817', '16:12:11', 'popo주점입니다.', 5, 1, 22, 34.0, 11.0, '333', 1, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(16, 456456, '456456', '청담류', '서울 강남구 도산대로58길 12', '02-260-0817', '16:12:11', 'popo주점입니다.', 5, 1, 22, 23.0, 11.0, '333', 0, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(17, 456456, '456456', '아실라', '서울 강남구 강남대로98길 16', '02-260-0817', '16:12:11', 'popo주점입니다.', 5, 1, 22, 523.0, 11.0, '333', 1, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(18, 456456, '1231251sdiuhgusi', '리커테이션', '서울 강남구 압구정로10길 13', '02-260-0817', '16:12:11', 'popo주점입니다.', 5, 1, 22, 23.0, 11.0, '333', 0, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(19, 456456, '1231251sdiuhgusi', '센도수산', '서울 강남구 역삼로65길 15', '02-260-0817', '16:12:11', 'popo주점입니다.', 1, 1, 22, 23.0, 11.0, '33', 1, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(20, 456456, '1231251sdiuhgusi', '심퍼티쿠시', '서울 강남구 논현로87길 19', '02-260-0817', '16:12:11', 'popo주점입니다.', 1, 1, 22, 23.0, 11.0, '333', 0, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(21, 456456, '1231251sdiuhgusi', '왕스펍', '서울 강남구 강남대로98길 11', '02-260-0817', '16:12:11', 'nnn', 4, 0, 11, 11.0, 11.0, '22', 0, 0);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(22, 456456, '1231251sdiuhgusi', '난지당', '서울 강남구 강남대로42길 16', '02-260-0817', '16:12:11', 'rrr', 3, 1, 22, 11.0, 11.0, '33', 1, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(23, 456456, '1231251sdiuhgusi', '꼭끄닭', '서울 강남구 봉은사로4길 37', '02-260-0817', '16:12:11', 'fff', 0, 0, 33, 33.0, 33.0, '33', 0, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(24, 456456, '1231251sdiuhgusi', '스페인클럽', '서울 강남구 압구정로10길 26', '02-260-0817', '16:12:11', '스페인', 3, 1, 11, 33.0, 33.0, '33', 1, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(25, 456456, '1231251sdiuhgusi', '하레이자카야', '서울 강남구 영동대로85길 38', '02-260-0817', '16:12:11', '하레이자카야', 1, 0, 33, 33.0, 33.0, '33', 0, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(26, 456456, '1231251sdiuhgusi', '오레노유메', '서울 강남구 압구정로2길 59', '02-260-0817', '16:12:11', '오레노유메', 2, 1, 33, 33.0, 33.0, '33', 1, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(27, 456456, '1231251sdiuhgusi', '고우', '서울 강남구 도산대로75길 12', '02-260-0817', '16:12:11', '고우', 3, 1, 33, 33.0, 33.0, '33', 0, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(28, 456456, '1231251sdiuhgusi', '겟 올라잇 ', '서울 강남구 도산대로57길 18', '02-260-0817', '16:12:11', '겟올라잇', 4, 0, 33, 33.0, 33.0, '33', 1, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(29, 456456, '1231251sdiuhgusi', '쇼린', '서울 강남구 도산대로81길 18', '02-260-0817', '16:12:11', '쇼린', 5, 1, 33, 33.0, 33.0, '33', 0, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(30, 456456, '1231251sdiuhgusi', '금복당', '서울 강남구 봉은사로68길 7', '02-260-0817', '16:12:11', '금복당', 5, 0, 33, 33.0, 33.0, '33', 1, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(31, 456456, '1231251sdiuhgusi', '아낙', '서울 강남구 학동로20길 30', '02-260-0817', '16:12:11', '아낙', 5, 1, 33, 33.0, 33.0, '33', 0, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(32, 456456, '1231251sdiuhgusi', '모던오뎅', '서울 강남구 도산대로17길 10', '02-260-0817', '16:12:11', '모던오뎅', 4, 0, 33, 33.0, 33.0, '33', 1, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(33, 456456, '1231251sdiuhgusi', '타이쇼', '서울 강남구 강남대로42길 16', '02-260-0817', '16:12:11', '타이쇼', 3, 1, 33, 33.0, 33.0, '33', 1, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(34, 456456, '1231251sdiuhgusi', '센야', '서울 강남구 역삼로9길 25', '02-260-0817', '16:12:11', '센야', 2, 0, 33, 33.0, 33.0, '33', 0, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(35, 456456, '1231251sdiuhgusi', '미라이', '서울 강남구 논현로153길 24', '02-260-0817', '16:12:11', '미라이', 1, 1, 33, 33.0, 33.0, '33', 1, 1);
+INSERT INTO sojudb.store
+(store_no, business_registration_no, business_registration, name, address, tel, `hour`, introduction, evaluation_score, reservation_accept, max_member, lat, lng, place_id, oper, status)
+VALUES(36, 456456, '1231251sdiuhgusi', '이자카야찬', '서울 강남구 논현로 634', '02-260-0817', '16:12:11', '이자카야찬', 0, 1, 33, 33.0, 33.0, '33', 1, 1);
