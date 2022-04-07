95419b12d4598af3519acb48ec51d1464765addb
supercode333 <gacomonkey123@gmail.com>
2022-04-07 18:34:51 +0900

    create asset folder

--- app/src/main/resources/static/party/partyList.js
+++ app/src/main/resources/static/party/partyList.js
@@ -6,47 +6,48 @@ var pbody = document.querySelector("#party-body")
     })
     .then(function(result) {      
       for (var party of result) {
-        var div = document.createElement("div");
-        div.innerHTML = `<div class="party-body-top">
-                            <div class="party-title">${party.title}</div>
-                            <div class="party-regdate">${party.regDate}</div>
-                        </div>
-                        <div class="party-body-content">
-                            <div class="leader-profile">
-                                <img class="profile-img leader" src="img/profile_suzy.jpg" alt="방장 프로필">
-                                <span>${Member.writer}</span>
+        pbody.innerHTML = `<div class="party-list">
+                            <div class="party-body-top">
+                                <div class="party-title">${party.title}</div>
+                                <div class="party-regdate">${party.regDate}</div>
                             </div>
-                            <div class="party-detail">
-                                <div class="first-column">
-                                    <span>
-                                        <i class="fa-solid fa-bottle-droplet"></i>소주</span>
-                                    <span>
-                                        <i class="fa-solid fa-whiskey-glass"></i>3병 이상</span>
+                            <div class="party-body-content">
+                                <div class="leader-profile">
+                                    <img class="profile-img leader" src="img/profile_suzy.jpg" alt="방장 프로필">
+                                    <span>${Member.writer}</span>
                                 </div>
-                                <div class="second-column">
-                                    <span>
-                                        <i class="fa-solid fa-calendar-days"></i>3월 3일(목) 23:00</span>
-                                    <span>
-                                        <i class="fa-solid fa-won-sign"></i>50,000,000,000원</span>
-                                </div>
-                                <div class="third-column">
-                                    <span>
-                                        <i class="fa-solid fa-user"></i>5명</span>
-                                    <span>
-                                        <i class="fa-solid fa-store"></i>50m
-                                    </span>
+                                <div class="party-detail">
+                                    <div class="first-column">
+                                        <span>
+                                            <i class="fa-solid fa-bottle-droplet"></i>소주</span>
+                                        <span>
+                                            <i class="fa-solid fa-whiskey-glass"></i>3병 이상</span>
+                                    </div>
+                                    <div class="second-column">
+                                        <span>
+                                            <i class="fa-solid fa-calendar-days"></i>3월 3일(목) 23:00</span>
+                                        <span>
+                                            <i class="fa-solid fa-won-sign"></i>50,000,000,000원</span>
+                                    </div>
+                                    <div class="third-column">
+                                        <span>
+                                            <i class="fa-solid fa-user"></i>5명</span>
+                                        <span>
+                                            <i class="fa-solid fa-store"></i>50m
+                                        </span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="party-body-footer">
-                        <div class="party-like">
-                            <i class="fa-solid fa-heart like-click"></i>
-                            1.3k
-                        </div>
-                        <div class="party-member-count">
-                            <i class="fa-solid fa-user"></i>
-                            <span>18</span>&nbsp;/&nbsp;<span>20</span>
+                            <div class="party-body-footer">
+                            <div class="party-like">
+                                <i class="fa-solid fa-heart like-click"></i>
+                                1.3k
+                            </div>
+                            <div class="party-member-count">
+                                <i class="fa-solid fa-user"></i>
+                                <span>18</span>&nbsp;/&nbsp;<span>20</span>
+                            </div>
                         </div>
                     </div>
         `;
