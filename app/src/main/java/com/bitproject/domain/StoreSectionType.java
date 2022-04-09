eb3cb480221da17133ff7a05242d7561a665d692
Seung-Bum <sb910126@gmail.com>
2022-04-10 01:03:59 +0900

    StoreType.java, StoreSectionType.java lombok 적용

--- app/src/main/java/com/bitproject/domain/StoreSectionType.java
+++ app/src/main/java/com/bitproject/domain/StoreSectionType.java
@@ -1,29 +1,13 @@
 package com.bitproject.domain;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
 public class StoreSectionType{
 
   int storeTypeNo;
   int storeNo;
-
-  @Override
-  public String toString() {
-    return "StoreSectionType [storeTypeNo=" + storeTypeNo + ", storeNo=" + storeNo + "]";
-  }
-
-
-  public int getStoreTypeNo() {
-    return storeTypeNo;
-  }
-  public void setStoreTypeNo(int storeTypeNo) {
-    this.storeTypeNo = storeTypeNo;
-  }
-  public int getStoreNo() {
-    return storeNo;
-  }
-  public void setStoreNo(int storeNo) {
-    this.storeNo = storeNo;
-  }
-
-
 
 }
--- app/src/main/java/com/bitproject/domain/StoreType.java
+++ app/src/main/java/com/bitproject/domain/StoreType.java
@@ -1,27 +1,13 @@
 package com.bitproject.domain;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
 public class StoreType{
 
   int storeTypeNo;
   String typeName;
-
-  @Override
-  public String toString() {
-    return "StoreType [storeTypeNo=" + storeTypeNo + ", typeName=" + typeName + "]";
-  }
-
-  public int getStoreTypeNo() {
-    return storeTypeNo;
-  }
-  public void setStoreTypeNo(int storeTypeNo) {
-    this.storeTypeNo = storeTypeNo;
-  }
-  public String getTypeName() {
-    return typeName;
-  }
-  public void setTypeName(String typeName) {
-    this.typeName = typeName;
-  }
-
 
 }
