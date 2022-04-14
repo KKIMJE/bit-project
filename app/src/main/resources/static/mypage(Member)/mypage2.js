bf276ea9a0af91fdbbf35a8f2e364af291e3500a
ture9200 <didtjddms09@naver.com>
2022-04-14 20:57:37 +0900

    2022-04-14 모달창

---
+++ app/src/main/resources/static/mypage(Member)/mypage2.js
@@ -0,0 +1,17 @@
+$().ready(function() {
+    $("#ceo-account-btn").click(function () {
+                Swal.fire({
+                    icon: 'success',
+                    text: '주점을 등록하면 사장님 계정이 생성됩니다.',
+                    showCancelButton:true,
+                    confirmButtonColor:'#90d483',
+                    cancelButtonColor: '#90d483',
+                    confirmButtonText:'들어가기',
+                    cancelButtonText:'나가기'
+                }).then((result) => {
+                  if(result.isConfirmed) {
+                    location.href = "../../memberBoss/MemberBoss.html";
+                  }
+                })
+            });
+        });
