36aebc7f08fcd4cc8bd11aeca63b0e1fcecf8722
ture9200 <didtjddms09@naver.com>
2022-04-15 20:58:13 +0900

    .

--- app/src/main/resources/static/memberBoss/MemberBoss.js
+++ app/src/main/resources/static/memberBoss/MemberBoss.js
@@ -65,10 +65,10 @@
     }
     
    
-      var xName = document.querySelector("input[name=name]");
+      var xStoreName = document.querySelector("input[name=storeName]");
       var xTel = document.querySelector("input[name=tel]");
-      var xHour = document.querySelector("input[name=hour]");
-        var xIntroduction = document.querySelector("input[name=introduction]");
+      var xHour = document.querySelector("textarea[name=hour]");
+        var xIntroduction = document.querySelector("textarea[name=introduction]");
          
 
       document.querySelector("form[name=login__form]").onsubmit = function() {
