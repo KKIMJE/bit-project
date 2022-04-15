8164cd462cd519fe1f6c77bb49f99ed08c3beb36
supercode333 <gacomonkey123@gmail.com>
2022-04-15 09:32:55 +0900

    우리지금만나 지도 수정

--- app/src/main/resources/static/party/partyMap.js
+++ app/src/main/resources/static/party/partyMap.js
@@ -1,3 +1,7 @@
+/******************
+    지도 생성  
+******************/
+
 var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 var options = { //지도를 생성할 때 필요한 기본 옵션
 	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
@@ -5,6 +9,12 @@ var options = { //지도를 생성할 때 필요한 기본 옵션
 };
 
 var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+
+
+/******************
+    컨트롤 생성  
+******************/
 
 // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
 var mapTypeControl = new kakao.maps.MapTypeControl();
@@ -16,3 +26,102 @@ map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
 // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
 var zoomControl = new kakao.maps.ZoomControl();
 map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+
+
+/**************************
+중심좌표에 따른 현재 주소 
+**************************/
+
+// 주소-좌표 변환 객체를 생성합니다
+var geocoder = new kakao.maps.services.Geocoder();
+
+var marker = new kakao.maps.Marker(), // 클릭한 위치를 표시할 마커입니다
+    infowindow = new kakao.maps.InfoWindow({zindex:1}); // 클릭한 위치에 대한 주소를 표시할 인포윈도우입니다
+
+// 지도를 클릭했을 때 클릭 위치 좌표에 대한 주소정보를 표시하도록 이벤트를 등록합니다
+kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
+    searchDetailAddrFromCoords(mouseEvent.latLng, function(result, status) {
+        if (status === kakao.maps.services.Status.OK) {
+            var detailAddr = '<div>' + result[0].address.address_name + '</div>';
+            
+            var content = '<div class="bAddr">' +
+                            detailAddr + 
+                        '</div>';
+
+            // 마커를 클릭한 위치에 표시합니다 
+            marker.setPosition(mouseEvent.latLng);
+
+            // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소정보를 표시합니다
+            infowindow.setContent(content);
+            infowindow.open(map, marker);
+        }   
+    });
+});
+
+// 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
+kakao.maps.event.addListener(map, 'idle', function() {
+    searchAddrFromCoords(map.getCenter(), displayCenterInfo);
+});
+
+function searchAddrFromCoords(coords, callback) {
+    // 좌표로 행정동 주소 정보를 요청합니다
+    geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);         
+}
+
+function searchDetailAddrFromCoords(coords, callback) {
+    // 좌표로 법정동 상세 주소 정보를 요청합니다
+    geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+}
+
+// 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
+function displayCenterInfo(result, status) {
+    if (status === kakao.maps.services.Status.OK) {
+        var infoDiv = document.getElementById('centerAddr');
+
+        for(var i = 0; i < result.length; i++) {
+            // 행정동의 region_type 값은 'H' 이므로
+            if (result[i].region_type === 'H') {
+                infoDiv.innerHTML = result[i].address_name;
+                break;
+            }
+        }
+    }    
+}
+
+
+
+
+
+// // 주소-좌표 변환 객체를 생성합니다
+// var geocoder = new kakao.maps.services.Geocoder();
+
+// var marker = new kakao.maps.Marker(), // 클릭한 위치를 표시할 마커입니다
+//     infowindow = new kakao.maps.InfoWindow({zindex:1}); // 클릭한 위치에 대한 주소를 표시할 인포윈도우입니다
+
+// var center = map.getCenter();
+
+// console.log(center.getLng());
+// console.log(center.getLat());
+
+// // 지도를 클릭했을 때 클릭 위치 좌표에 대한 주소정보를 표시하도록 이벤트를 등록합니다
+// kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
+//     searchDetailAddrFromCoords(mouseEvent.latLng, function(result, status) {
+//         if (status === kakao.maps.services.Status.OK) {
+//             var detailAddr = !!result[0].road_address ? '<div>도로명주소 : ' + result[0].road_address.address_name + '</div>' : '';
+//             detailAddr += '<div>지번 주소 : ' + result[0].address.address_name + '</div>';
+            
+//             var content = '<div class="bAddr">' +
+//                             '<span class="title">법정동 주소정보</span>' + 
+//                             detailAddr + 
+//                         '</div>';
+
+//             // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소정보를 표시합니다
+//             infowindow.setContent(content);
+//             infowindow.open(map, marker);
+//         }   
+//     });
+// });
+
+
+
