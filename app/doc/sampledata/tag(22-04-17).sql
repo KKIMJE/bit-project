7d6111c9db8041b86ac69b1e331eb6e4fd5dbafc
Seung-Bum <sb910126@gmail.com>
2022-04-17 23:55:46 +0900

    tag 테이블 데이터 insert, sql문 추가

---
+++ app/doc/sampledata/tag(22-04-17).sql
@@ -0,0 +1,91 @@
+--태그
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(1, '소주');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(2, '맥주');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(3, '막걸리');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(4, '양주');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(5, '와인');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(6, '사케');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(7, '전통주');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(8, '칵테일');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(9, 'HOT');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(10, '조용한');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(11, '혼술');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(12, '이국적인');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(13, '파티');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(14, '회식');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(15, '룸');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(16, '아늑한');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(17, '역세권');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(18, '현지인');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(19, '헌팅');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(20, '분위기좋은');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(21, '데이트');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(22, '가성비');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(23, '노래가좋은');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(24, '맛난안주');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(25, '야외');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(26, '노포');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(27, '#주점태그27');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(28, '#주점태그28');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(29, '#주점태그29');
+INSERT INTO sojudb.tag
+(tag_no, name)
+VALUES(30, '#주점태그30');
