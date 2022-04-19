7c1629bf503dced529942aa0a4d32396d8835c10
kinnatzzang <shinich13@naver.com>
2022-04-19 18:27:39 +0900

    스윗얼랏 팝업창

--- app/src/main/resources/static/memberBoss/MemberBoss.js
+++ app/src/main/resources/static/memberBoss/MemberBoss.js
@@ -1,10 +1,10 @@
-$().ready(function () {
-  $("#add_tag").click(function () {
-    const { value: fruit } = await Swal.fire({
-      title: 'Select field validation',
+
+  $("#add_tag").click(async function () {
+    const { value: 태그 } = await Swal.fire({
+      title: '사장님 가게에 어울리는 태그를 선택하세요',
       input: 'select',
       inputOptions: {
-        'Fruits': {
+        '태그선택': {
            apples: 'Apples',
            bananas: 'Bananas',
            grapes: 'Grapes',
@@ -17,14 +17,14 @@ $().ready(function () {
         },
         'icecream': 'Ice cream'
       },
-      inputPlaceholder: 'Select a fruit',
+      inputPlaceholder: '태그 선택하기',
       showCancelButton: true,
       inputValidator: (value) => {
         return new Promise((resolve) => {
           if (value === 'oranges') {
             resolve()
           } else {
-            resolve('You need to select oranges :)')
+            resolve('적어도 하나의 태그를 선택하셔야 합니다 :)')
           }
         })
       }
@@ -40,7 +40,6 @@ $().ready(function () {
   var xTel = document.querySelector("input[name=tel]");
   var xHour = document.querySelector("textarea[name=hour]");
   var xIntroduction = document.querySelector("textarea[name=introduction]");
-      
 
   document.querySelector("form[name=login__form]").onsubmit = function() {
     /*if(xNam.value == ""  
@@ -50,7 +49,7 @@ $().ready(function () {
           return false;
          }*/
 
-  var fd = new FormData(document.forms.namedItem("login__form"));
+    var fd = new FormData(document.forms.namedItem("login__form"));
     fetch("/store/add",{ 
       method: "POST",
       body: new URLSearchParams(fd)
@@ -61,16 +60,18 @@ $().ready(function () {
     })
     .then(function(result) {
       console.log(result);
-        /* if (result.status == "success") {
-           alert("성공했습니다")
-            } else {
-              window.alert("실패!!");
-            }*/
+      if (result.status == "success") {
+        alert("성공했습니다")
+      } else {
+        window.alert("실패!!");
+      }
     });
   
           
     document.querySelector("#exit").onclick = function() {
-    window.location.href = "../ceo/storemanagement.html";
+      window.location.href = "../ceo/storemanagement.html";
     };
+
+  }
 
       
