4806ece4ad3e545b6a51bcfa5f3592c9dec627d1
kinnatzzang <shinich13@naver.com>
2022-04-19 19:35:48 +0900

    팝업창 수정중

--- app/src/main/resources/static/memberBoss/MemberBoss.js
+++ app/src/main/resources/static/memberBoss/MemberBoss.js
@@ -5,17 +5,18 @@
       input: 'select',
       inputOptions: {
         '태그선택': {
-           apples: 'Apples',
-           bananas: 'Bananas',
-           grapes: 'Grapes',
-           oranges: 'Oranges'
+           아늑한: '#아늑한',
+           역세권: '역세권',
+           현지인: '현지인',
+           헌팅: '헌팅',
         },
-        'Vegetables': {
-           potato: 'Potato',
-           broccoli: 'Broccoli',
-           carrot: 'Carrot'
-        },
-        'icecream': 'Ice cream'
+           분위기좋은: '분위기좋은',
+           데이트: '데이트',
+           가성비: '가성비',
+           노래가좋은: '노래가좋은',
+           맛난안주: '맛난안주',
+           야외: '야외',
+           노포: '노포'
       },
       inputPlaceholder: '태그 선택하기',
       showCancelButton: true,
@@ -28,27 +29,26 @@
           }
         })
       }
-    })
-
-    if (fruit) {
-      Swal.fire(`You selected: ${fruit}`)
-    }
   })
-  
    
   var xStoreName = document.querySelector("input[name=storeName]");
   var xTel = document.querySelector("input[name=tel]");
   var xHour = document.querySelector("textarea[name=hour]");
   var xIntroduction = document.querySelector("textarea[name=introduction]");
+  var xBusinessRegistrationNo = document.querySelector("input[name=businessRegistrationNo]");
+  var xAddressKakao = document.querySelector("input[name=AddressKakao]");
 
   document.querySelector("form[name=login__form]").onsubmit = function() {
-    /*if(xNam.value == ""  
-         xEmail.value == "" 
-         xPassword.value == "") {
-         window.alert("필수 입력 항목이 비어 있습니다.");
-          return false;
-         }*/
-
+    if(xStoreName.value == "" || 
+       xTel.value == "" ||
+       xBusinessRegistrationNo == "" ||
+       xIntroduction == "" ||
+       xAddressKakao == "" ||
+       xHour.value == "") {
+       window.alert("필수 입력 항목이 비어 있습니다.");
+         return false;
+       }
+         
     var fd = new FormData(document.forms.namedItem("login__form"));
     fetch("/store/add",{ 
       method: "POST",
@@ -74,4 +74,4 @@
 
   }
 
-      
+    }) 
