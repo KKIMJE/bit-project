3a1838a9dc942ce10b70c7f5a1c7037780d14553
ture9200 <didtjddms09@naver.com>
2022-04-19 19:23:15 +0900

    .

--- app/src/main/resources/static/memberBoss/MemberBoss.js
+++ app/src/main/resources/static/memberBoss/MemberBoss.js
@@ -1,63 +1,21 @@
 
-
-$().ready(function () {s
-$("#add_tag").click(function () {
-const { value: fruit } = Swal.fire({
-  title: 'Select field validation',
-  input: 'select',
-  inputOptions: {
-    'Fruits': {
-      apples: 'Apples',
-      bananas: 'Bananas',
-      grapes: 'Grapes',
-      oranges: 'Oranges'
-    },
-    'Vegetables': {
-      potato: 'Potato',
-      broccoli: 'Broccoli',
-      carrot: 'Carrot'
-    },
-    'icecream': 'Ice cream'
-  },
-  inputPlaceholder: 'Select a fruit',
-  showCancelButton: true,
-  inputValidator: (value) => {
-    return new Promise((resolve) => {
-      if (value === 'oranges') {
-        resolve()
-      } else {
-        resolve('You need to select oranges :)')
-      }
-    })
-  }
-});
-
-if (fruit) {
-  Swal.fire(`You selected: ${fruit}`)
-}
-
-   function sample6_execDaumPostcode() {
-        new daum.Postcode({
-            oncomplete: function(data) {
-                // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
-
   $("#add_tag").click(async function () {
     const { value: 태그 } = await Swal.fire({
       title: '사장님 가게에 어울리는 태그를 선택하세요',
       input: 'select',
       inputOptions: {
         '태그선택': {
-           apples: 'Apples',
-           bananas: 'Bananas',
-           grapes: 'Grapes',
-           oranges: 'Oranges'
-        },
-        'Vegetables': {
-           potato: 'Potato',
-           broccoli: 'Broccoli',
-           carrot: 'Carrot'
-        },
-        'icecream': 'Ice cream'
+           아늑한: '#아늑한',
+           역세권: '역세권',
+           현지인: '현지인',
+           헌팅: '헌팅',
+           분위기좋은: '분위기좋은',
+           데이트: '데이트',
+           가성비: '가성비',
+           노래가좋은: '노래가좋은',
+           맛난안주: '맛난안주',
+           야외: '야외',
+           노포: '노포'
       },
       inputPlaceholder: '태그 선택하기',
       showCancelButton: true,
@@ -70,27 +28,30 @@ if (fruit) {
           }
         })
       }
-    })
 
-    if (fruit) {
-      Swal.fire(`You selected: ${fruit}`)
+         if (fruit) {
+           Swal.fire(`You selected: ${fruit}`)
     }
   })
-  
    
   var xStoreName = document.querySelector("input[name=storeName]");
   var xTel = document.querySelector("input[name=tel]");
   var xHour = document.querySelector("textarea[name=hour]");
   var xIntroduction = document.querySelector("textarea[name=introduction]");
+  var xBusinessRegistrationNo = document.querySelector("input[name=businessRegistrationNo]");
+  var xAddressKakao = document.querySelector("input[name=AddressKakao]");
 
   document.querySelector("form[name=login__form]").onsubmit = function() {
-    /*if(xNam.value == ""  
-         xEmail.value == "" 
-         xPassword.value == "") {
-         window.alert("필수 입력 항목이 비어 있습니다.");
-          return false;
-         }*/
-
+    if(xStoreName.value == "" || 
+       xTel.value == "" ||
+       xBusinessRegistrationNo == "" ||
+       xIntroduction == "" ||
+       xAddressKakao == "" ||
+       xHour.value == "") {
+       window.alert("필수 입력 항목이 비어 있습니다.");
+         return false;
+       }
+         
     var fd = new FormData(document.forms.namedItem("login__form"));
     fetch("/store/add",{ 
       method: "POST",
@@ -115,12 +76,4 @@ if (fruit) {
     };
 
   }
-
-      document.querySelector("#exit").onclick = function() {
-        window.location.href = "../main/main.html";
-      };
-    
-    document.querySelector("#next").onclick = function() {
-        window.location.href = "../ceo/storemanagement.html";
-      };
 
