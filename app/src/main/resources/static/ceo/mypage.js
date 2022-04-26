081aadf04359917d4cb73c45837fb97ec6172b26
ture9200 <didtjddms09@naver.com>
2022-04-26 15:57:40 +0900

    2022-04-26

--- app/src/main/resources/static/ceo/mypage.js
+++ app/src/main/resources/static/ceo/mypage.js
@@ -1,20 +1,5 @@
-// category-tab
-const lightBtn = document.querySelector('.roomlist-category-sort');
-lightBtn.addEventListener("click",function(e){
-  if (e.target == e.currentTarget) {
-    return;c
-  } else {
-    e.currentTarget.querySelector('.act').classList.toggle('act');
-    e.target.classList.toggle('act');
-  }
-});
-
-//next, pre button
-const next = document.querySelector('.next');
-const pre = document.querySelector('.previous');
-
 $().ready(function() {
-    $("#enter").click(function () {
+    $("#management-btn").click(function () {
                 Swal.fire({
                     icon: 'warning',
                     text: '선택해주세요!',
