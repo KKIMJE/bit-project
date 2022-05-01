10047d577033c5bda5816b7dac968e1e5f6a8fc1
KJEzorro <gndlg91@gmail.com>
2022-05-02 03:47:48 +0900

    사장님 추가 버튼 수정

--- app/src/main/resources/static/member/mypage.js
+++ app/src/main/resources/static/member/mypage.js
@@ -1,63 +1,60 @@
+//var xMno = document.querySelector("input[name=mno]");
+// 이미지 추가해야함
+var xNickname = document.querySelector(".my-nickname");
+var xEmail = document.querySelector(".my-email");
+let xMemberPhoto = document.querySelector(".my-profile")
+let ceoBtnDiv = document.querySelector(".ceo-btn-div")
 
-    $(document).on("click", "#ceo-account-btn", () => {
-                Swal.fire({
-                    icon: 'success',
-                    text: '주점을 등록하면 사장님 계정이 생성됩니다.',
-                    showCancelButton:true,
-                    confirmButtonColor:'#90d483',
-                    cancelButtonColor: '#90d483',
-                    confirmButtonText:'들어가기',
-                    cancelButtonText:'나가기'
-                }).then((result) => {
-                  if(result.isConfirmed) {
-                    location.href = "../../memberBoss/MemberBoss.html";
-                  }
-                })
-            });
+fetch("/member/getLoginUser")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(result) {
+    // 4) 상세 정보를 화면에 출력한다.
+    if (result.status == "fail") {
+      window.alert("서버 요청 오류!");
+      console.log(result);
+      return;
+    }
+    console.log(result.data);
+    var member = result.data;
+    xNickname.innerHTML = member.nickName;
+    xEmail.innerHTML = member.email;
+    console.log(member.mimg);
+    $(xMemberPhoto).attr("src", `/member/photo?filename=150x150_${member.mimg}`);
+    if (member.mimg == null) {
+      $(xMemberPhoto).attr("src", "/member/photo?filename=150x150_default_general_member.png");
+    }
 
-
-
-
- //var xMno = document.querySelector("input[name=mno]");
-  // 이미지 추가해야함
-  var xNickname = document.querySelector(".my-nickname");
-  var xEmail = document.querySelector(".my-email");
-  let xMemberPhoto = document.querySelector(".my-profile")
-  let ceoBtnDiv = document.querySelector(".ceo-btn-div")
-
- fetch("/member/getLoginUser")
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(result) {
-        // 4) 상세 정보를 화면에 출력한다.
-        if (result.status == "fail") {
-          window.alert("서버 요청 오류!");
-          console.log(result);
-          return;
-        }
-        console.log(result.data);
-        var member = result.data;
-        xNickname.innerHTML = member.nickName;
-        xEmail.innerHTML = member.email;
-        console.log(member.mimg);
-        $(xMemberPhoto).attr("src", `/member/photo?filename=150x150_${member.mimg}`);
-        if (member.mimg == null) {
-          $(xMemberPhoto).attr("src", "/member/photo?filename=150x150_default_general_member.png");
-        }
-
-        let addCeo = `<button type="button" id="ceo-account-btn">사장님 계정 추가하기</button>`
-        let ceoPage = `<button type="button" class="ceo-page-btn" onclick="location.href='/ceo/mypage.html'">사장님 페이지 이동</button>`
+    let addCeo = `<button type="button" id="ceo-account-btn">사장님 계정 추가하기</button>`
+    let ceoPage = `<button type="button" class="ceo-page-btn" onclick="location.href='/ceo/mypage.html'">사장님 페이지 이동</button>`
 
 
-        if (member.storeCount == 0) {
-          ceoBtnDiv.innerHTML = addCeo
-        } else {
-          ceoBtnDiv.innerHTML = ceoPage
-        }
-          });
+    if (member.storeCount == 0) {
+      ceoBtnDiv.innerHTML = addCeo
+    } else {
+      ceoBtnDiv.innerHTML = ceoPage
+    }
+  });
+
+
+$(document).on("click", "#ceo-account-btn", () => {
+  Swal.fire({
+    icon: 'success',
+    text: '주점을 등록하면 사장님 계정이 생성됩니다.',
+    showCancelButton: true,
+    confirmButtonColor: '#90d483',
+    cancelButtonColor: '#90d483',
+    confirmButtonText: '들어가기',
+    cancelButtonText: '나가기'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      location.href = "../../memberBoss/MemberBoss.html";
+    }
+  })
+});
 
 
 
 
-        // sns계정추가해야함
+// sns계정추가해야함
